58fd1b4b2dafc9c16e6e8572a5b5c0b1f5d69415
David <david@tangentdigitalagency.com>
2020-12-21 12:25:00 -0700

    Added new titles for script

--- src/components/S9Final.jsx
+++ src/components/S9Final.jsx
@@ -19,7 +19,7 @@ class S9Final extends Component {
 		return (
 			
 			<div className="container pt-0 main-content-container">
-				<h1 className="center"> You Are On Your Way To Saving!</h1>
+				<h1 className="center"> You Are On Your Way To Saving on Your <b>Business</b> Insurance</h1>
 				<br></br>
                 <div id="target"> 
 				<UncontrolledLottie />
